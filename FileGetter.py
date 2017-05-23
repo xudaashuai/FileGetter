6150e33d900e8e33a651e0daa35f2f1de6eda8f5
xudaashuai <xudashuai0819@outlook.com>
2017-05-23 23:19:11 +0800

    1

---
+++ FileGetter.py
@@ -0,0 +1,17 @@
+
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, jsonify, request,send_from_directory,abort
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory('',filename,as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run()
